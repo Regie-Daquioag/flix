d64e1556eceb1ed84b7536e592f9d309a10ac9d9
Regie-Daquioag <rdaquiong@csumb.edu>
2018-02-11 22:42:54 -0800

    added week 2 changes and the readme update

---
+++ flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Regie Daquioag on 2/5/18.
+//  Copyright © 2018 Regie Daquioag. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
